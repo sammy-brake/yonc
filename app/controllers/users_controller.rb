d6365c1b21eceb3cbc0978369a410aa8c7d4bf55
Samantha brake <sammy.brake@gmail.com>
2019-03-20 19:43:16 +0000

    add login ability

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,11 @@ class UsersController < ApplicationController
   end
 
   get "/login" do
+    if !logged_in?
     erb :"/users/login"
+    else
+      redirect "/users/:name"
+    end
   end
 
   get "/users/:name" do
@@ -17,15 +21,25 @@ class UsersController < ApplicationController
     if params[:name] == "" || params[:password_digest] == ""
       redirect :"/signup"
     else
-
       User.all.each do |user|
         if user.name == params[:name]
           redirect :"/signup"
         end
       end
       @user = User.create(params)
+      session[:user_id] = @user.id
       redirect :"/users/#{@user.name}"
     end
   end
+
+  post '/login' do
+  @user = User.find_by(:name => params[:name])
+  if @user && @user.authenticate(password_digest: params[:password])
+    session[:user_id] = user.id
+    redirect "/users/#{@user.name}"
+  else
+    redirect to '/'
+  end
+end
 
 end
