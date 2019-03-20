c932043e2ad05ecc5d99c563d0774e0acf32d39b
Samantha brake <sammy.brake@gmail.com>
2019-03-20 19:27:58 +0000

    add signup ability and block same username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,17 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    erb :"/index"
+  end
 
+  helpers do
+    def logged_in?
+      !!current_user
+    end
 
-  erb :"/index"
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,25 @@ class UsersController < ApplicationController
   get "/login" do
     erb :"/users/login"
   end
-  ## later on, change functionality to have user slug
-  get "/user" do
+
+  get "/users/:name" do
+    @user = User.find_by(name: params[:name])
     erb :"/users/show"
+  end
+
+  post "/signup" do
+    if params[:name] == "" || params[:password_digest] == ""
+      redirect :"/signup"
+    else
+
+      User.all.each do |user|
+        if user.name == params[:name]
+          redirect :"/signup"
+        end
+      end
+      @user = User.create(params)
+      redirect :"/users/#{@user.name}"
+    end
   end
 
 end
